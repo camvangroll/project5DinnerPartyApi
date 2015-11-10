b1f94288e74bd803ccfbd0dcaf84df50523c0f40
camvangroll <camille.vangroll@gmail.com>
2015-11-10 16:52:11 -0500

    made updates

--- dinner-party.js
+++ dinner-party.js
@@ -1,8 +1,8 @@
 
 var dinnerParty = {};
 
-dinnerParty.appId = '4180c144';
-dinnerParty.appKey = '58c4ec16abb8bc536b83d95dbb8c80b2';
+dinnerParty.appId = 'fec46765';
+dinnerParty.appKey = 'e0af6f1300b348c2a9d2c3df08f3e1a6';
 dinnerParty.maincourses = "course^course-Main%20Dishes";
 dinnerParty.apps = "course^course-Appetizers";
 dinnerParty.desserts = "course^course-Desserts";
@@ -23,10 +23,33 @@ dinnerParty.desserts = "course^course-Desserts";
 	 		maxResult: "1"
 	 	}
 	}).then(function(res){
-		dinnerParty.displayRecipes(res);
 
+		var recipeId = res.matches[0].id;
+
+		//Calling a new function that will hold our results and put the ID to use
+
+		dinnerParty.getInfoMains(recipeId);
+
+
+		});
+}
+
+dinnerParty.getInfoMains = function(idSent){
+	$.ajax({
+		url: 'http://api.yummly.com/v1/api/recipe/'+ idSent,
+		method: 'GET',
+		dataType: 'jsonp',
+		data: {
+			_app_key: dinnerParty.appKey,
+	 		_app_id: dinnerParty.appId
+		}
+	}).then(function(res) {
+		console.log(res);
+		dinnerParty.displayRecipes(res);
 	});
 }
+
+
 
 dinnerParty.getDesserts = function(allergy,holiday) {
 	$.ajax({
@@ -42,9 +65,32 @@ dinnerParty.getDesserts = function(allergy,holiday) {
 	 		maxResult: "1"
 		}
 	}).then(function(res){
+
+		$.each(res.matches, function(index, value) {
+
+			var recipeId = value.id;
+
+			dinnerParty.getInfoDesserts(recipeId);
+	});
+});
+
+}
+
+dinnerParty.getInfoDesserts = function(idSent){
+	$.ajax({
+		url: 'http://api.yummly.com/v1/api/recipe/'+ idSent,
+		method: 'GET',
+		dataType: 'jsonp',
+		data: {
+			_app_key: dinnerParty.appKey,
+	 		_app_id: dinnerParty.appId
+		}
+	}).then(function(res) {
 		dinnerParty.displayRecipes(res);
 	});
 }
+
+
 
 dinnerParty.getApps = function(allergy,holiday) {
 	$.ajax({
@@ -60,31 +106,50 @@ dinnerParty.getApps = function(allergy,holiday) {
 	 		maxResult: "1"
 		}
 	}).then(function(res){
+
+		$.each(res.matches, function(index, value) {
+
+			var recipeId = value.id;
+
+			dinnerParty.getInfoApps(recipeId);
+	});
+});
+
+}
+
+dinnerParty.getInfoApps = function(idSent){
+	$.ajax({
+		url: 'http://api.yummly.com/v1/api/recipe/'+ idSent,
+		method: 'GET',
+		dataType: 'jsonp',
+		data: {
+			_app_key: dinnerParty.appKey,
+	 		_app_id: dinnerParty.appId
+		}
+	}).then(function(res) {
 		dinnerParty.displayRecipes(res);
 	});
 }
 
-dinnerParty.populateGenres = function() {
-	$.ajax({
-		url: 'http://developer.echonest.com/api/v4/genre/list?api_key=NGHRLRLHEFDPL3VY8&format=json',
-		method: 'GET',
-		dataType: 'json'
-	}).then(function(res) {
-		for(var i = 0; i < res.response.genres.length; i++) {
-			$('select#item').append('<option value="' + res.response.genres[i].name + '">' + res.response.genres[i].name + '</option>');
-		}
-	});
-};
+// dinnerParty.populateGenres = function() {
+// 	$.ajax({
+// 		url: 'http://developer.echonest.com/api/v4/genre/list?api_key=NGHRLRLHEFDPL3VY8&format=json',
+// 		method: 'GET',
+// 		dataType: 'json'
+// 	}).then(function(res) {
+// 		for(var i = 0; i < res.response.genres.length; i++) {
+// 			$('select#item').append('<option value="' + res.response.genres[i].name + '">' + res.response.genres[i].name + '</option>');
+// 		}
+// 	});
+// }
 
 dinnerParty.getArtists = function(res) {
-	console.log("call and get artists");
 	var userGenre = $('#item').val();
 	$.ajax({
 		url: "http://developer.echonest.com/api/v4/genre/artists?api_key=NGHRLRLHEFDPL3VY8&format=json&results=5&bucket=hotttnesss&bucket=images&bucket=urls&name=" + userGenre, 
 		method: 'GET',
 		dataType: 'json'
 	}).then(function(res){
-		console.log(res);
 		dinnerParty.displayArtists(res);
 	});
 }
@@ -93,24 +158,23 @@ dinnerParty.getArtists = function(res) {
 dinnerParty.formSubmit = function() {
 	$('form').on('submit', function(e){
 		e.preventDefault();
+
 		$('.suggestionWrapper').show();
+
 		var numberOfPeople = $('#people').val();
 		var allergy = $('#DietaryRestrictions').val();
 		var holiday = $('input[name=holiday]:checked').val();
-		console.log("inside form")
-
+		
+		$('section.recipes').empty();
 		if (numberOfPeople < 3) {
-	 		console.log("less than three");
 		 	dinnerParty.getMains(allergy,holiday);
 		 }
 		 if (numberOfPeople == 3 || numberOfPeople == 4){
-		 		console.log("equal to 3 or 4");
 		 		dinnerParty.getMains(allergy,holiday);
 		 		dinnerParty.getDesserts(allergy,holiday);
 		 }
 
 	 	if (numberOfPeople > 4) {
-	 		console.log("5 or above");
 	 		dinnerParty.getApps(allergy,holiday);
 	 		dinnerParty.getMains(allergy,holiday);
 	 		dinnerParty.getDesserts(allergy,holiday);
@@ -120,24 +184,25 @@ dinnerParty.formSubmit = function() {
 
 	 	$('html,body').animate({scrollTop:$("#answer").offset().top}, 'slow');
 	});
-};
+}
 
 
 dinnerParty.displayRecipes = function(res){
-		console.log("im in display recipe!")
-		console.log(res.matches[0]);
-		var recipeName = $('<h2>').text(res.matches[0].recipeName);
-		var recipeImage = $('<img>').attr('src', res.matches[0].smallImageUrls);
-		var ingredients = $('<ul>').text(res.matches[0].ingredients);
-		var source = $('<a>').attr('href', res.matches[0].sourceDisplayName).text("Get Recipe");
-		var container = $('<div>').append(recipeName, recipeImage, ingredients, source);
+		var recipeName = $('<h2>').text(res.name);
+		var recipeImage = $('<img>').attr('src', res.images[0].imageUrlsBySize["360"]);
+		var ingredients = $('<ul>');
+		for (var i = 0; i < res.ingredientLines.length; i++) {
+			var ingredient = $('<li>').text(res.ingredientLines[i]);
+			ingredients.append(ingredient);
+		}
+		var attribution = $('<p>').append(res.attribution.html);
+		var container = $('<div>').append(recipeName, recipeImage, ingredients, attribution);
 		var recipesSection = $('section.recipes').append(container);
 
-};
+}
 
 dinnerParty.displayArtists = function(artwork){
-	console.log("im in display artists!")
-	console.log(artwork);
+	$('section.artists').empty();
 	$.each(artwork.response.artists, function(i, item) {
 		var artistTitle = $('<h2>').text(item.name);
 		// var artistImage = $('<img>').attr('src', item.images[0].url);
@@ -157,16 +222,15 @@ dinnerParty.formRefresh = function() {
 
 
 dinnerParty.init = function() {
-	dinnerParty.populateGenres();
 	dinnerParty.formSubmit();
 	dinnerParty.formRefresh();
 	
 };
 
 
-$(function(){
+$(function() {
 	dinnerParty.init();
-}); 
+});
 
 
 
